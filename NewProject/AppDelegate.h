e27d36b61c56cf468f5c699c3e4c3f18dc8e4fa2
qzhm2017 <qzhm1987@163.com>
2017-09-22 11:27:20 +0800

    添加项目文件适配iPhone X

---
+++ NewProject/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  NewProject
+//
+//  Created by Mac on 2017/9/22.
+//  Copyright © 2017年 BeiJingXiaoMenTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewProject/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  NewProject
+//
+//  Created by Mac on 2017/9/22.
+//  Copyright © 2017年 BeiJingXiaoMenTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
